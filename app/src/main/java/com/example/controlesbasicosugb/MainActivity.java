707565e6d297295b60fddff0043fb2dbf6f9d778
MINEDUCYT <canaleskeyla002@gmail.com>
2024-02-02 13:58:30 -0600

    Creacion de la rama conversores

--- app/src/main/java/com/example/controlesbasicosugb/MainActivity.java
+++ app/src/main/java/com/example/controlesbasicosugb/MainActivity.java
@@ -10,87 +10,19 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tempVal;
-    Button btn;
-    RadioGroup opt;
-    Spinner spn;
+
+    TabHot tbh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tempVal = findViewById(R.id.txtnum1);
-                double num1 = Double.parseDouble(tempVal.getText().toString());
+        tbh * findViewById(R.id.tbhconversor);
+        tbh.setup();
 
-                tempVal = findViewById(R.id.txtnum2);
-                double num2 = Double.parseDouble(tempVal.getText().toString());
+        tbh.addTab(tbh.newTabSpec(tap"LON").setContent(R.id.tbhLongitud).setIndicator(label "LONGITUD", icon null));
+        tbh.addTab(tbh.newTabSpec(tap"ALM").setContent(R.id.tbhAlmacenamiento).setIndicator(label "ALMACENAMIENTO", icon null));
+        tbh.addTab(tbh.newTabSpec(tap"MON").setContent(R.id.tbhMonedas).setIndicator(label "MONEDAS", icon null));
 
-                double respuesta = 0;
-                opt = findViewById(R.id.optOpciones);
-                switch (opt.getCheckedRadioButtonId()){
-                    case R.id.optSuma:
-                        respuesta = num1+num2;
-                        break;
-                    case R.id.optResta:
-                        respuesta=num1-num2;
-                        break;
-                    case R.id.optMultiplicacion:
-                        respuesta=num1*num2;
-                        break;
-                    case R.id.optDivision:
-                        respuesta=num1/num2;
-                        break;
-                    case R.id.optPorcentaje:
-                        respuesta = (num1 / 100) * num2;
-                        break;
-                    case R.id.optExponenciacion:
-                        respuesta = Math.pow(num1, num2);
-                        break;
-                    case R.id.optFactorial:
-                        respuesta = factorial((int) num1);
-                        break;
-                    case R.id.optRaiz:
-                        respuesta = Math.sqrt(num1);
-                        break;
-                }
-
-                spn = findViewById(R.id.spnOpciones);
-                int selectedSpinnerItemPosition = spn.getSelectedItemPosition();
-
-                switch (selectedSpinnerItemPosition) {
-                    case 0:
-                        respuesta = num1 + num2;
-                        break;
-                    case 1:
-                        respuesta = num1 - num2;
-                        break;
-                    case 2:
-                        respuesta = num1 * num2;
-                        break;
-                    case 3:
-                        respuesta = num1 / num2;
-                        break;
-                    // Nuevos casos del Spinner
-                }
-
-                tempVal = findViewById(R.id.lblrespuesta);
-                tempVal.setText("Respuesta: " + respuesta);
-            }
-        });
-    }
-
-    private double factorial(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        double result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
     }
 }
